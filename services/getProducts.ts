0afb31b09f647cbb461b3b1bd665fbaabbc58adc
Salvador Bacci <salvador_bacci@hotmail.com>
2024-01-07 17:04:53 -0300

    improved database query

--- services/getProducts.ts
+++ services/getProducts.ts
@@ -1,7 +1,6 @@
 import { yellow, red } from 'colorette'
 import { dbConnect } from 'lib/mongooseConnection'
 import Products from 'models/products'
-import { productsObjects } from 'constants/static_products'
 import { MongoProduct, Product } from '@/types/types'
 import { cache } from 'react'
 
@@ -10,15 +9,13 @@ dbConnect()
 const getProducts = cache(async () => {
   console.log(yellow('GET /products'))
   try {
-    const response = await Products.find().lean<MongoProduct[]>()
-    const mongoProducts: Product[] = response.map(({ _id, ...product }) => product)
-    const products: Product[] = mongoProducts.length > 0 ? mongoProducts : productsObjects
-    const filteredProducts: Product[] = products.filter(product => product.available)
+    const response = await Products.find({ available: true }).lean<MongoProduct[]>()
+    const products: Product[] = response.map(({ _id, ...product }) => product)
 
-    return filteredProducts
+    return products
   } catch (err) {
     console.log(red(`Error on GET /products: ${err}`))
-    return (productsObjects as Product[]).filter((product) => product.available)
+    return []
   }
 })
 
